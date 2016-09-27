e3c0a38a3f631c409a4179cbd12d63271be18f0b
FabioUR <feur27gen@gmail.com>
2016-09-26 19:39:39 -0600

    Add files via upload

--- Generador_Letras.v
+++ Generador_Letras.v
@@ -1,12 +1,11 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 		Fabio Ureña Rojas
+// Company: ITCR
+// Engineer: Fabio Ureña Rojas
 // 
-// Create Date:    12:55:16 09/21/2016 
-// Design Name: 
-// Module Name:    Generador_Letras 
-// Project Name: 
+// Create Date:    19:01:34 08/16/2016 
+// Design Name:    
+// Module Name:    Generador_Pixeles.
+// Project Name:   Controlador_VGA.
 // Target Devices: 
 // Tool versions: 
 // Description: 
@@ -15,12 +14,13 @@
 //
 // Revision: 
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Generador_Letras(
-   //ENTRADAS
+module B(
+	//ENTRADAS
 	input wire CLK,
+	input wire RESET,
 	//input wire [3:0] digitDec_DD, digitUni_DD, digitDec_M, digitUni_M, digitDec_AN, digitUni_AN,
 	//digitDec_HORA, digitUni_HORA, digitDec_MIN, digitUni_MIN, digitDec_SEG, digitUni_SEG,
 	//digitDec_TimerHORA, digitUni_TimerHORA, digitDec_TimerMIN, digitUni_TimerMIN, digitDec_TimerSEG, digitUni_TimerSEG,
@@ -44,28 +44,37 @@ module Generador_Letras(
 	wire [3:0] row_addr_FECHA, row_addr_NumFECHA, row_addr_HORA, row_addr_NumHORA, row_addr_ForMili, row_addr_TIMER, row_addr_NumTIMER, row_addr_SIMBOLO; //fila (y)
    wire [2:0] bit_addr_FECHA, bit_addr_NumFECHA, bit_addr_HORA, bit_addr_NumHORA, bit_addr_ForMili, bit_addr_TIMER, bit_addr_NumTIMER, bit_addr_SIMBOLO; //bit (x)
 	wire FECHA_on, NumFECHA_on, HORA_on, NumHORA_on, ForMili_on, TIMER_on, NumTIMER_on, SIMBOLO_on; //establecera valor booleano como indicador que se pintara palabra FECHA
-	
+	reg [6:0] char_addr_FECHA_reg;
 	//Instanciar FONT ROM
 	font_rom font_unit
       (.clk(CLK), .addr(rom_addr), .data(font_word));
 	
-	//1. Definir el espacio y las letras correspondientes a la palabra FECHA 32x32
-	assign FECHA_on = ((pix_y[9:5]==1) && (pix_x[9:5]>=9) && (pix_x[9:5]<=11)); //Me difine el tamaño y=2^5 y x=2^5
-	assign row_addr_FECHA = pix_y[5:1]; //me define el tamaño de la letra
+	//1. Definir el espacio y las letras correspondientes a la palabra FECHA 16x32
+	assign FECHA_on = ((pix_y[9:5]==1) && (pix_x[9:4]>=18) && (pix_x[9:4]<=22)); //Me difine el tamaño y=2^5 y x=2^5
+	assign row_addr_FECHA = pix_y[4:1]; //me define el tamaño de la letra
 	assign bit_addr_FECHA = pix_x[4:1]; //me define el tamaño de la letra
 	
+	always @(posedge CLK, posedge RESET) begin
+			if(RESET)
+				begin
+				char_addr_FECHA_reg <= 0;
+				end
+			else
+				begin
+				char_addr_FECHA_reg <= char_addr_FECHA;
+				end
+	end
+	
 	always @* 
-	begin
 		case(pix_x[6:4]) //para este caso cada 2^4 bits se pinta nueva letra
 							  //coordenadas definidas dependiendo de las coordenadas especificadas anteriormente en FECHA_on
-			3'b010: char_addr_FECHA = 7'h46; //F
-			3'b011: char_addr_FECHA = 7'h45; //E
-			3'b100: char_addr_FECHA = 7'h43; //C
-			3'b101: char_addr_FECHA = 7'h48; //H
-			3'b110: char_addr_FECHA = 7'h41; //A
-			default: char_addr_FECHA = 7'h00;//Espacio en blanco
+			4'h2: char_addr_FECHA = 7'h46; //F
+			4'h3: char_addr_FECHA = 7'h45; //E
+			4'h4: char_addr_FECHA = 7'h43; //C
+			4'h5: char_addr_FECHA = 7'h48; //H
+			4'h6: char_addr_FECHA = 7'h41; //A
+			default: char_addr_FECHA = 7'h20;//Espacio en blanco
 		endcase
-	end
 
 	//2. Mostrar digitos de la fecha 64x32
 	assign NumFECHA_on = (pix_y[9:5]<=3) && (pix_y[9:5]>=2) && (pix_x[9:6]>=3) && (pix_x[9:6]<=6); //coordenadas donde se pintara los digitos
@@ -75,14 +84,14 @@ module Generador_Letras(
 	always@*
 	begin
 		case(pix_x[7:5]) //coordenadas definidas dependiendo de las coordenadas especificadas anteriormente en NumFECHA_on
-			3'b110: char_addr_NumFECHA = 7'h30; /*{3'b011, digitDec_DD};//(decenas dia)*/
-			3'b111: char_addr_NumFECHA = 7'h31;/*{3'b011, digitUni_DD};//(unidades dia)*/
-			3'b000: char_addr_NumFECHA = 7'h2f;// /
-			3'b001: char_addr_NumFECHA = 7'h32;/*{3'b011, digitDec_M};//(decenas Mes)*/
-			3'b010: char_addr_NumFECHA = 7'h33;/*{3'b011, digitUni_M};//(unidades mes)*/
-			3'b011: char_addr_NumFECHA = 7'h2f;// /
-			3'b100: char_addr_NumFECHA = 7'h34;/*{3'b011, digitDec_AN};//(unidad de millar año)*/
-			3'b101: char_addr_NumFECHA = 7'h35;/*{3'b011, digitUni_AN};//(Centenas año)*/
+			3'h6: char_addr_NumFECHA = 7'h30; /*{3'b011, digitDec_DD};//(decenas dia)*/
+			3'h7: char_addr_NumFECHA = 7'h31;/*{3'b011, digitUni_DD};//(unidades dia)*/
+			3'h0: char_addr_NumFECHA = 7'h2f;// /
+			3'h1: char_addr_NumFECHA = 7'h32;/*{3'b011, digitDec_M};//(decenas Mes)*/
+			3'h2: char_addr_NumFECHA = 7'h33;/*{3'b011, digitUni_M};//(unidades mes)*/
+			3'h3: char_addr_NumFECHA = 7'h2f;// /
+			3'h4: char_addr_NumFECHA = 7'h34;/*{3'b011, digitDec_AN};//(unidad de millar año)*/
+			3'h5: char_addr_NumFECHA = 7'h35;/*{3'b011, digitUni_AN};//(Centenas año)*/
 			default: char_addr_NumFECHA = 7'h00;//Espacio en blanco
 		endcase	
 	end
@@ -96,19 +105,18 @@ module Generador_Letras(
 	begin
 		case(pix_x[5:4]) //para este caso cada 2^4 bits se pinta nueva letra
 							  //coordenadas definidas dependiendo de las coordenadas especificadas anteriormente en HORA_on
-			2'b00: char_addr_HORA = 7'h48; //H
-			2'b01: char_addr_HORA = 7'h4f; //O
-			2'b10: char_addr_HORA = 7'h52; //R
-			2'b11: char_addr_HORA = 7'h41; //A
+			2'h0: char_addr_HORA = 7'h48; //H
+			2'h1: char_addr_HORA = 7'h4f; //O
+			2'h2: char_addr_HORA = 7'h52; //R
+			2'h3: char_addr_HORA = 7'h41; //A
 			default: char_addr_HORA = 7'h00;//Espacio en blanco
 			
 		endcase
 	end
 	
-	//4. Mostrar Palabra 24 H
+	//4. Mostrar Palabra AM/PM
 	//Crear posteriormente con una variable AM/PM, para seleccionar si se esta en AM/PM, usandolo un case,
 	//en este caso, posiblemente con un switch
-	//cambiar
 	assign ForMili_on = ((pix_y[9:5]==7) && (pix_x[9:5]>=9) && (pix_x[9:5]<=10)); //Me difine el tamaño y=2^5 y x=2^5
 	assign row_addr_ForMili = pix_y[5:1]; //me define el tamaño de la letra
 	assign bit_addr_ForMili = pix_x[4:1]; //me define el tamaño de la letra
@@ -134,15 +142,14 @@ module Generador_Letras(
 	always@*
 	begin
 		case(pix_x[6:4]) //coordenadas definidas dependiendo de las coordenadas especificadas anteriormente en NumHORA_on
-		
-			3'b000: char_addr_NumHORA = 7'h30; /*{3'b011, digitDec_HORA};//(decenas dia)*/
-			3'b001: char_addr_NumHORA = 7'h31;/*{3'b011, digitUni_HORA};//(unidades dia)*/
-			3'b010: char_addr_NumHORA = 7'h3a;// /
-			3'b011: char_addr_NumHORA = 7'h32;/*{3'b011, digitDec_MIN};//(decenas Mes)*/
-			3'b100: char_addr_NumHORA = 7'h33;/*{3'b011, digitUni_MIN};//(unidades mes)*/
-			3'b101: char_addr_NumHORA = 7'h3a;// /
-			3'b110: char_addr_NumHORA = 7'h34;/*{3'b011, digitDec_SEG};//(unidad de millar año)*/
-			3'b111: char_addr_NumHORA = 7'h35;/*{3'b011, digitUni_SEG};//(Centenas año)*/
+			3'h0: char_addr_NumHORA = 7'h30; /*{3'b011, digitDec_HORA};//(decenas dia)*/
+			3'h1: char_addr_NumHORA = 7'h31;/*{3'b011, digitUni_HORA};//(unidades dia)*/
+			3'h2: char_addr_NumHORA = 7'h3a;// /
+			3'h3: char_addr_NumHORA = 7'h32;/*{3'b011, digitDec_MIN};//(decenas Mes)*/
+			3'h4: char_addr_NumHORA = 7'h33;/*{3'b011, digitUni_MIN};//(unidades mes)*/
+			3'h5: char_addr_NumHORA = 7'h3a;// /
+			3'h6: char_addr_NumHORA = 7'h34;/*{3'b011, digitDec_SEG};//(unidad de millar año)*/
+			3'h7: char_addr_NumHORA = 7'h35;/*{3'b011, digitUni_SEG};//(Centenas año)*/
 			default: char_addr_NumHORA = 7'h00;//Espacio en blanco
 		endcase	
 	end
@@ -160,6 +167,7 @@ module Generador_Letras(
 			3'h6: char_addr_TIMER = 7'h4d; //M
 			3'h7: char_addr_TIMER = 7'h45; //E
 			3'h0: char_addr_TIMER = 7'h52; //R
+			default: char_addr_TIMER = 7'h00;//Espacio en blanco
 			
 		endcase
 	end
@@ -172,14 +180,14 @@ module Generador_Letras(
 	always@*
 	begin
 		case(pix_x[6:4]) //coordenadas definidas dependiendo de las coordenadas especificadas anteriormente en NumTIMER_on
-			3'h0: char_addr_NumTIMER = 7'h30; /*{3'b011, digitDec_TimerHORA};//(decenas dia)*/
-			3'h1: char_addr_NumTIMER = 7'h31;/*{3'b011, digitUni_TimerHORA};//(unidades dia)*/
+			3'h0: char_addr_NumTIMER = 7'h30; /*{3'b011, digitDec_HORA};//(decenas dia)*/
+			3'h1: char_addr_NumTIMER = 7'h31;/*{3'b011, digitUni_HORA};//(unidades dia)*/
 			3'h2: char_addr_NumTIMER = 7'h3a;// /
-			3'h3: char_addr_NumTIMER = 7'h32;/*{3'b011, digitDec_TimerMIN};//(decenas Mes)*/
-			3'h4: char_addr_NumTIMER = 7'h33;/*{3'b011, digitUni_TimerMIN};//(unidades mes)*/
+			3'h3: char_addr_NumTIMER = 7'h32;/*{3'b011, digitDec_MIN};//(decenas Mes)*/
+			3'h4: char_addr_NumTIMER = 7'h33;/*{3'b011, digitUni_MIN};//(unidades mes)*/
 			3'h5: char_addr_NumTIMER = 7'h3a;// /
-			3'h6: char_addr_NumTIMER = 7'h34;/*{3'b011, digitDec_TimerSEG};//(unidad de millar año)*/
-			3'h7: char_addr_NumTIMER = 7'h35;/*{3'b011, digitUni_TimerSEG};//(Centenas año)*/
+			3'h6: char_addr_NumTIMER = 7'h34;/*{3'b011, digitDec_SEG};//(unidad de millar año)*/
+			3'h7: char_addr_NumTIMER = 7'h35;/*{3'b011, digitUni_SEG};//(Centenas año)*/
 			default: char_addr_NumTIMER = 7'h00;//Espacio en blanco
 		endcase	
 	end
@@ -191,24 +199,27 @@ module Generador_Letras(
 
 	always@*
 	begin
-		case(pix_x[5]) //coordenadas definidas dependiendo de las coordenadas especificadas anteriormente en SIMBOLO_on
-			1'h0: char_addr_SIMBOLO = 7'h06; //simbolo de espadas
-			default: char_addr_SIMBOLO = 7'h00;//Espacio en blanco
+		case(pix_x[9:5]) //coordenadas definidas dependiendo de las coordenadas especificadas anteriormente en SIMBOLO_on
+			5'd16: char_addr_SIMBOLO = 7'h06; //simbolo de espadas
+			//default: char_addr_SIMBOLO = 7'h00;//Espacio en blanco
 		endcase	
 	end
 	
+	assign rom_addr = {char_addr, row_addr};
+   assign font_bit = font_word[~bit_addr];
+	
 	//Mostrar en pantalla las letras con los colores definidos
 	//Multiplexar las direcciones del font ROM con salida RBG
-	always @* begin
+	always @(posedge CLK) begin
 		
-		graph_rgb <= 3'b000;
+		graph_rgb = 3'b000;
 		if (FECHA_on) //palabra FECHA
 		begin
 			char_addr = char_addr_FECHA;
 			row_addr = row_addr_FECHA;
 			bit_addr = bit_addr_FECHA;
 				if (font_bit) begin
-					graph_rgb <= 3'b010; //verde
+					graph_rgb = 3'b010; //verde
 				end
 		end
 		
@@ -218,7 +229,7 @@ module Generador_Letras(
 			row_addr = row_addr_NumFECHA;
 			bit_addr = bit_addr_NumFECHA;
 				if (font_bit) begin
-					graph_rgb <= 3'b111; //blanco
+					graph_rgb = 3'b111; //blanco
 				end
 		end
 		
@@ -228,7 +239,7 @@ module Generador_Letras(
 			row_addr = row_addr_HORA;
 			bit_addr = bit_addr_HORA;
 				if (font_bit) begin
-					graph_rgb <= 3'b010; //verde
+					graph_rgb = 3'b010; //verde
 				end
 		end
 		
@@ -238,17 +249,17 @@ module Generador_Letras(
 			row_addr = row_addr_NumHORA;
 			bit_addr = bit_addr_NumHORA;
 				if (font_bit) begin
-					graph_rgb <= 3'b111; //blanco
+					graph_rgb = 3'b111; //blanco
 				end
 		end
 		
-		else if (ForMili_on) //Palabra 24 H. Posteriormente se hara cambio
+		else if (ForMili_on) //Palabra AM/PM. Posteriormente se hara cambio
 		begin
 			char_addr = char_addr_ForMili;
 			row_addr = row_addr_ForMili;
 			bit_addr = bit_addr_ForMili;
 				if (font_bit) begin
-					graph_rgb <= 3'b111; //blanco
+					graph_rgb = 3'b111; //blanco
 				end
 		end
 		
@@ -258,7 +269,7 @@ module Generador_Letras(
 			row_addr = row_addr_TIMER;
 			bit_addr = bit_addr_TIMER;
 				if (font_bit) begin
-					graph_rgb <= 3'b010; //verde
+					graph_rgb = 3'b010; //verde
 				end
 		end
 		
@@ -268,7 +279,7 @@ module Generador_Letras(
 			row_addr = row_addr_NumTIMER;
 			bit_addr = bit_addr_NumTIMER;
 				if (font_bit) begin
-					graph_rgb <= 3'b111;  //blanco
+					graph_rgb = 3'b111;  //blanco
 				end
 		end
 		
@@ -278,16 +289,13 @@ module Generador_Letras(
 			row_addr = row_addr_SIMBOLO;
 			bit_addr = bit_addr_SIMBOLO;
 				if (font_bit) begin
-					graph_rgb <= 3'b111;   //blanco
+					graph_rgb = 3'b111;   //blanco
 				end
 		end
 		
 		else begin
-			graph_rgb <= 3'b000; //negro
+			graph_rgb = 3'b000; //negro
 		end
 	end
-	
-	assign rom_addr = {char_addr, row_addr};
-   assign font_bit = font_word[~bit_addr];
 	
 endmodule
