7d8c21a81b28df442f40cb8b05940e00d0f1e027
Leon-SA <sleon08@hotmail.es>
2016-09-29 15:27:02 -0600

    Add files via upload

--- FSM_LEER_RTC.v
+++ FSM_LEER_RTC.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    01:28:59 09/23/2016 
+// Create Date:    15:20:04 09/29/2016 
 // Design Name: 
 // Module Name:    FSM_LEER_RTC 
 // Project Name: 
@@ -19,37 +19,44 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module FSM_LEER_RTC(
-   input wire clk, reset,
+	input wire clk, reset,
 	input wire do_it_leer,
 	
 	output wire a_d, cs, rd, wr, // Señales de ctrl RTC.
 	
-	output reg rtc_to_ram,
-	output reg ram_to_rtc,
+	output reg rtc_to_reg,
+	output reg reg_to_rtc,
 	
-	output reg dir_ram_com_cyt,
 	
-	output reg dir_ram_seg,
-	output reg dir_ram_dir_seg,
+	output reg dir_com_cyt,
 	
-	output reg dir_ram_min,
-	output reg dir_ram_dir_min,
+	output reg dat_seg,
+	output reg dir_seg,
 	
-	output reg dir_ram_hora,
-	output reg dir_ram_dir_hora,
+	output reg dat_min,
+	output reg dir_min,
 	
-	output reg dir_ram_dia,
-	output reg dir_ram_dir_dia,
+	output reg dat_hora,
+	output reg dir_hora,
 	
-	output reg dir_ram_mes,
-	output reg dir_ram_dir_mes,
+	output reg dat_dia,
+	output reg dir_dia,
 	
-	output reg dir_ram_anio,
-	output reg dir_ram_dir_anio,
+	output reg dat_mes,
+	output reg dir_mes,
 	
-	output reg w_ram_enable,
-	output reg r_ram_enable
-    );
+	output reg dat_anio,
+	output reg dir_anio,
+	
+	output reg dat_seg_tim,
+	output reg dir_seg_tim,
+	
+	output reg dat_min_tim,
+	output reg dir_min_tim,
+	
+	output reg dat_hora_tim,
+	output reg dir_hora_tim
+   );
 	
 	reg w_r;
 	reg do_it;
@@ -76,30 +83,31 @@ module FSM_LEER_RTC(
 	
 	reg est_sig;
 	reg est_act;
-	reg [7:0] Contador;
+	reg [8:0] contador;
 	
 	/* Lógica Secuencial */
 	
-	always @(posedge clk, posedge reset)
-	begin
-		if (reset) begin
-			est_act <= est0;
-		end else	if (Contador > 245) begin
-			est_act <= est0;
-		end else begin
-			est_act <= est_sig;
-		end
-	end
-	
 	always @(posedge clk) begin
 		if (est_act == est0) begin
-			Contador <= 8'b00000000;
+			contador <= 9'b000000000;
 		end else	begin
-			Contador <= Contador + 8'b00000001;
+			contador <= contador + 9'b000000001;
 		end
 	end
 	
-		/* Lógica Combinacional */
+	always @(posedge clk, posedge reset) begin
+		if (reset) begin
+			est_act <= est0;
+		end else begin
+			if (contador > 350) begin
+				est_act <= est0;
+			end else begin
+				est_act <= est_sig;
+			end
+		end
+	end
+	
+	/* Lógica Combinacional */
 	
 	always @* begin
 		est_sig = est0;
@@ -112,7 +120,11 @@ module FSM_LEER_RTC(
 				end
 			end
 			est1: begin
-				est_sig = est1;
+				if (do_it_leer) begin
+					est_sig = est1;
+				end else begin
+					est_sig = est0;
+				end
 			end
 			default: est_sig = est0;
 		endcase
@@ -124,278 +136,333 @@ module FSM_LEER_RTC(
 		if (est_act == est0) begin
 			w_r = 0;
 			do_it = 0;
-			rtc_to_ram = 0;
-			ram_to_rtc = 0;
-			w_ram_enable = 0;
-			r_ram_enable = 0;
-			dir_ram_com_cyt = 0;
-			dir_ram_dir_seg = 0;
-			dir_ram_seg = 0;
-			dir_ram_dir_min = 0;
-			dir_ram_min = 0;
-			dir_ram_dir_hora = 0;
-			dir_ram_hora = 0;
-			dir_ram_dir_dia = 0;
-			dir_ram_dia = 0;
-			dir_ram_dir_mes = 0;
-			dir_ram_mes = 0;
-			dir_ram_dir_anio = 0;
-			dir_ram_anio = 0;
+			rtc_to_reg = 0;
+			reg_to_rtc = 0;
+			
+			dir_com_cyt = 0;
+			dat_seg = 0;
+			dir_seg = 0;
+			dat_min = 0;
+			dir_min = 0;
+			dat_hora = 0;
+			dir_hora = 0;
+			dat_dia = 0;
+			dir_dia = 0;
+			dat_mes = 0;
+			dir_mes = 0;
+			dat_anio = 0;
+			dir_anio = 0;
+			dat_seg_tim = 0;
+			dir_seg_tim = 0;
+			dat_min_tim = 0;
+			dir_min_tim = 0;
+			dat_hora_tim = 0;
+			dir_hora_tim = 0;
 		end
 		else if (est_act == est1) begin
 			w_r = 0;
 			do_it = 1;
 			if (send_add && (~send_data) && (~read_data)) begin
-				rtc_to_ram = 0;
-				ram_to_rtc = 1;
-				w_ram_enable = 0;
-				r_ram_enable = 1;
-				if (Contador > 210) begin
-					dir_ram_dir_anio = 1;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 175) begin
-					dir_ram_dir_mes = 1;
-					dir_ram_mes = 0;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 140) begin
-					dir_ram_dir_dia = 1;
-					dir_ram_dia = 0;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 105) begin
-					dir_ram_dir_hora = 1;
-					dir_ram_hora = 0;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 70) begin
-					dir_ram_dir_min = 1;
-					dir_ram_min = 0;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 35) begin
-					dir_ram_dir_seg = 1;
-					dir_ram_seg = 0;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_com_cyt = 0;
+				rtc_to_reg = 0;
+				reg_to_rtc = 1;
+				
+				dat_seg = 0;
+				dat_min = 0;
+				dat_hora = 0;
+				dat_dia = 0;
+				dat_mes = 0;
+				dat_anio = 0;
+				dat_seg_tim = 0;
+				dat_min_tim = 0;
+				dat_hora_tim = 0;
+				if (350 >= contador > 315) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 1;
+				end else	if (315 >= contador > 280) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 1;
+					dir_hora_tim = 0;
+				end else	if (280 >= contador > 245) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 1;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
+				end else if (245 >= contador > 210) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 1;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
+				end else if (210 >= contador > 175) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 1;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
+				end else if (175 >= contador > 140) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 1;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
+				end else if (140 >= contador > 105) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 1;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
+				end else if (105 >= contador > 70) begin
+					dir_com_cyt = 0;
+					dir_seg = 0;
+					dir_min = 1;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
+				end else if (70 >= contador > 35) begin
+					dir_com_cyt = 0;
+					dir_seg = 1;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
 				end else begin
-					dir_ram_com_cyt = 1;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
+					dir_com_cyt = 1;
+					dir_seg = 0;
+					dir_min = 0;
+					dir_hora = 0;
+					dir_dia = 0;
+					dir_mes = 0;
+					dir_anio = 0;
+					dir_seg_tim = 0;
+					dir_min_tim = 0;
+					dir_hora_tim = 0;
 				end
 			end else if (read_data && (~send_add) && (~send_data)) begin
-				rtc_to_ram = 1;
-				ram_to_rtc = 0;
-				w_ram_enable = 1;
-				r_ram_enable = 0;
-				if (Contador > 210) begin
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 1;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 175) begin
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 1;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 140) begin
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 1;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador >105) begin
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 1;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 70) begin
-					dir_ram_dir_min = 0;
-					dir_ram_min = 1;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
-					dir_ram_com_cyt = 0;
-				end else if (Contador > 35) begin
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 1;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_com_cyt = 0;
+				rtc_to_reg = 1;
+				reg_to_rtc = 0;
+				
+				dir_seg = 0;
+				dir_min = 0;
+				dir_hora = 0;
+				dir_dia = 0;
+				dir_mes = 0;
+				dir_anio = 0;
+				dir_seg_tim = 0;
+				dir_min_tim = 0;
+				dir_hora_tim = 0;
+				if (350 >= contador > 315) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 1;
+				end else if (315 >= contador > 280) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 1;
+					dat_hora_tim = 0;
+				end else if(280 >= contador > 245) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 1;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
+				end else if (245 >= contador > 210) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 1;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
+				end else if (210 >= contador > 175) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 1;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
+				end else if (175 >= contador > 140) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 1;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
+				end else if (140 >= contador > 105) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 1;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
+				end else if (105 >= contador > 70) begin
+					dir_com_cyt = 0;
+					dat_seg = 0;
+					dat_min = 1;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
+				end else if (70 >= contador > 35) begin
+					dir_com_cyt = 0;
+					dat_seg = 1;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
 				end else begin
-					dir_ram_com_cyt = 1;
-					dir_ram_dir_anio = 0;
-					dir_ram_anio = 0;
-					dir_ram_dir_mes = 0;
-					dir_ram_mes = 0;
-					dir_ram_dir_dia = 0;
-					dir_ram_dia = 0;
-					dir_ram_dir_hora = 0;
-					dir_ram_hora = 0;
-					dir_ram_dir_min = 0;
-					dir_ram_min = 0;
-					dir_ram_dir_seg = 0;
-					dir_ram_seg = 0;
+					dir_com_cyt = 1;
+					dat_seg = 0;
+					dat_min = 0;
+					dat_hora = 0;
+					dat_dia = 0;
+					dat_mes = 0;
+					dat_anio = 0;
+					dat_seg_tim = 0;
+					dat_min_tim = 0;
+					dat_hora_tim = 0;
 				end
 			end else begin
-				w_ram_enable = 0;
-				r_ram_enable = 0;
-				rtc_to_ram = 0;
-				ram_to_rtc = 0;
-				dir_ram_com_cyt = 0;
-				dir_ram_dir_seg = 0;
-				dir_ram_seg = 0;
-				dir_ram_dir_min = 0;
-				dir_ram_min = 0;
-				dir_ram_dir_hora = 0;
-				dir_ram_hora = 0;
-				dir_ram_dir_dia = 0;
-				dir_ram_dia = 0;
-				dir_ram_dir_mes = 0;
-				dir_ram_mes = 0;
-				dir_ram_dir_anio = 0;
-				dir_ram_anio = 0;
+				w_r = 0;
+				do_it = 1;
+				rtc_to_reg = 0;
+				reg_to_rtc = 0;
+				
+				dir_com_cyt = 0;
+				dat_seg = 0;
+				dir_seg = 0;
+				dat_min = 0;
+				dir_min = 0;
+				dat_hora = 0;
+				dir_hora = 0;
+				dat_dia = 0;
+				dir_dia = 0;
+				dat_mes = 0;
+				dir_mes = 0;
+				dat_anio = 0;
+				dir_anio = 0;
+				dat_seg_tim = 0;
+				dir_seg_tim = 0;
+				dat_min_tim = 0;
+				dir_min_tim = 0;
+				dat_hora_tim = 0;
+				dir_hora_tim = 0;
 			end
 		end
 		else begin
 			w_r = 0;
 			do_it = 0;
-			rtc_to_ram = 0;
-			ram_to_rtc = 0;
-			w_ram_enable = 0;
-			r_ram_enable = 0;
-			dir_ram_com_cyt = 0;
-			dir_ram_dir_seg = 0;
-			dir_ram_seg = 0;
-			dir_ram_dir_min = 0;
-			dir_ram_min = 0;
-			dir_ram_dir_hora = 0;
-			dir_ram_hora = 0;
-			dir_ram_dir_dia = 0;
-			dir_ram_dia = 0;
-			dir_ram_dir_mes = 0;
-			dir_ram_mes = 0;
-			dir_ram_dir_anio = 0;
-			dir_ram_anio = 0;
+			rtc_to_reg = 0;
+			reg_to_rtc = 0;
+			
+			dir_com_cyt = 0;
+			dat_seg = 0;
+			dir_seg = 0;
+			dat_min = 0;
+			dir_min = 0;
+			dat_hora = 0;
+			dir_hora = 0;
+			dat_dia = 0;
+			dir_dia = 0;
+			dat_mes = 0;
+			dir_mes = 0;
+			dat_anio = 0;
+			dir_anio = 0;
+			dat_seg_tim = 0;
+			dir_seg_tim = 0;
+			dat_min_tim = 0;
+			dir_min_tim = 0;
+			dat_hora_tim = 0;
+			dir_hora_tim = 0;
 		end
 	end
-		
+
 endmodule
-
-
